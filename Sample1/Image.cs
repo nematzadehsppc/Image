2446d4dea1f379ed803b42f90d72a3b2d3152cd5
Behzad Nematzadeh <b_nematzadeh@sppc.co.ir>
2019-04-07 16:04:16 +0430

    Add project files.

---
+++ Sample1/Image.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tadbir
+{
+    public class Image : System.Activities.CodeActivity<System.Drawing.Image>
+    {
+        public InArgument<string> Port { get; set; }
+
+        public InArgument<string> dbName { get; set; }
+
+        public InArgument<int> MerchId { get; set; }
+
+        public InArgument<int> FPId { get; set; }
+
+        public InArgument<string> ServiceKey { get; set; }
+
+        protected override System.Drawing.Image Execute(CodeActivityContext context)
+        {
+            try
+            {
+                string port = Port.Get<string>(context);
+                string db = dbName.Get<string>(context);
+                int merchId = MerchId.Get<int>(context);
+                int fpid = FPId.Get<int>(context);
+                string serviceKey = ServiceKey.Get<string>(context);
+
+                string url = string.Format("http://130.185.76.7:{0}/{1}/V3.0/Inventory/Image/GetImageById?MerchId={2}&SysId=4&FormId=2&FPId={3}&ShowAll=0&key={4}", port, db, merchId, fpid, serviceKey);
+
+                HttpClient client = new HttpClient();
+                Task<HttpResponseMessage> message = client.GetAsync(url);
+                message.Wait();
+                Task<string> messageResult = message.Result.Content.ReadAsStringAsync();
+                messageResult.Wait();
+
+                System.Drawing.Image result = null;
+                byte[] imageByteArray = Convert.FromBase64String(messageResult.Result.Replace("\"", ""));
+                using (MemoryStream stream = new MemoryStream(imageByteArray))
+                {
+                    result = System.Drawing.Image.FromStream(stream);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+
+    public class ImageID : System.Activities.CodeActivity<System.Drawing.Image>
+    {
+        public InArgument<string> Port { get; set; }
+
+        public InArgument<string> dbName { get; set; }
+
+        public InArgument<int> MerchId { get; set; }
+
+        public InArgument<int> FPId { get; set; }
+
+        public InArgument<string> ServiceKey { get; set; }
+
+        public InArgument<int> ImageId { get; set; }
+
+
+        protected override System.Drawing.Image Execute(CodeActivityContext context)
+        {
+            try
+            {
+                string port = Port.Get<string>(context);
+                string db = dbName.Get<string>(context);
+                int merchId = MerchId.Get<int>(context);
+                int fpid = FPId.Get<int>(context);
+                string serviceKey = ServiceKey.Get<string>(context);
+                int imageId = ImageId.Get<int>(context);
+
+                string url = string.Format("http://130.185.76.7:{0}/{1}/V3.0/Inventory/Image/getImageByImageId?MerchId={2}&SysId=4&FormId=2&FPId={3}&key={4}&ImageId={5}", port, db, merchId, fpid, serviceKey, imageId);
+
+                HttpClient client = new HttpClient();
+                Task<HttpResponseMessage> message = client.GetAsync(url);
+                message.Wait();
+                Task<string> messageResult = message.Result.Content.ReadAsStringAsync();
+                messageResult.Wait();
+
+                System.Drawing.Image result = null;
+                byte[] imageByteArray = Convert.FromBase64String(messageResult.Result.Replace("\"", ""));
+                using (MemoryStream stream = new MemoryStream(imageByteArray))
+                {
+                    result = System.Drawing.Image.FromStream(stream);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Sample2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Sample2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxRequest = new System.Windows.Forms.PictureBox();
+            this.buttonRequest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRequest)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxRequest
+            // 
+            this.pictureBoxRequest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxRequest.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxRequest.Name = "pictureBoxRequest";
+            this.pictureBoxRequest.Size = new System.Drawing.Size(776, 387);
+            this.pictureBoxRequest.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRequest.TabIndex = 0;
+            this.pictureBoxRequest.TabStop = false;
+            // 
+            // buttonRequest
+            // 
+            this.buttonRequest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRequest.Location = new System.Drawing.Point(12, 405);
+            this.buttonRequest.Name = "buttonRequest";
+            this.buttonRequest.Size = new System.Drawing.Size(75, 23);
+            this.buttonRequest.TabIndex = 1;
+            this.buttonRequest.Text = "Request";
+            this.buttonRequest.UseVisualStyleBackColor = true;
+            this.buttonRequest.Click += new System.EventHandler(this.buttonRequest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRequest);
+            this.Controls.Add(this.pictureBoxRequest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRequest)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxRequest;
+        private System.Windows.Forms.Button buttonRequest;
+    }
+}
+
